88fb0b3b808ff53ce75aabfd21590ce0f3174b8f
levita <levitagoodlvns@gmail.com>
2025-10-04 19:57:56 -0500

    upload first files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    //production: true,
+    appVersion: '1.0.0',
+    isMockEnabled: true,
+    appRealm: 'municusco',
+    appClientId: '',
+    //apiUrl: 'https://api-convenciones.cusco.gob.pe/center-medical/v1',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    appVersion: '1.0.0',
+    appRealm: 'municusco',
+    appClientId: '',
+    //apiUrl: 'https://api-convenciones.cusco.gob.pe/center-medical/v1',
+    apiUrl: 'http://localhost:8000/center-medical/v1'
+};
