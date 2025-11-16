17769667cd1860a3ac2f0dabf2175ed7e76b1288
levita <levitagoodlvns@gmail.com>
2025-11-16 18:32:44 -0500

    update services to medical

--- src/app/components/medical/medical.routes.ts
+++ src/app/components/medical/medical.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+import { MedicalServicesComponent } from './services/services.component';
 
 export const MEDICAL_ROUTES: Routes = [
   // { path: '', component: MedicalDashboardComponent },
+  { path: 'services', component: MedicalServicesComponent },
 ];
-
