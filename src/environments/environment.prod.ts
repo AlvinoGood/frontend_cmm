ef03cfbd036331d46d35ed9956b56c90eeed18eb
levita <levitagoodlvns@gmail.com>
2025-11-19 03:21:55 -0500

    update apiUrl to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
-    //production: true,
-    appVersion: '1.0.0',
-    isMockEnabled: true,
-    appRealm: 'municusco',
-    appClientId: '',
+    production: true,
+    //appVersion: '1.0.0',
+    //isMockEnabled: true,
+    //appRealm: 'municusco',
+    //appClientId: '',
     //apiUrl: 'https://api-convenciones.cusco.gob.pe/center-medical/v1',
+    apiUrl: 'https://backend-cmm.onrender.com/medical-center/v1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    appVersion: '1.0.0',
-    appRealm: 'municusco',
-    appClientId: '',
+    //appVersion: '1.0.0',
+    //appRealm: 'municusco',
+    //appClientId: '',
     //apiUrl: 'https://api-convenciones.cusco.gob.pe/center-medical/v1',
     apiUrl: 'http://localhost:3000/medical-center/v1'
 };
